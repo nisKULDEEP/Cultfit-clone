11edacc464df18904896fa9a61a9400aa7c82a5a
Bhavisha2801 <nayibhavisha@gmail.com>
2022-04-06 20:56:41 +0530

    modified care page

--- src/Components/Care/SuperSpecialists.jsx
+++ src/Components/Care/SuperSpecialists.jsx
@@ -1,34 +1,21 @@
 import React from 'react'
+import "./Care.scss";
 
 const SuperSpecialists = () => {
   return (
-    <div className='specialities superspecialists'>
-      <div className='specialities__heading'>
-        <h1 className='specialities__head'>Super Specialists</h1>
-        <p className='specialities__para'>Video/Audio call super-specialists for your family</p>
+    <div className='superspecialists'>
+      <div className='superspecialists__heading'>
+        <h1 className='superspecialists__head'>Super Specialists</h1>
+        <p className='superspecialists__para'>Video/Audio call super-specialists for your family</p>
       </div>
-      <div className='specialities__images'>
-        <div>
-            <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,ar_0.8,fl_progressive,f_auto,q_auto:eco/dpr_2/image/singles/care/tc/GastroEnterologist.png" alt="" className='specialities__IndImage' />
-        </div>
-        <div>
-            <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,ar_0.8,fl_progressive,f_auto,q_auto:eco/dpr_2/image/singles/care/tc/ENDO.jpg" alt="" className='specialities__IndImage' />
-        </div>
-        <div>
-            <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,ar_0.8,fl_progressive,f_auto,q_auto:eco/dpr_2/image/singles/care/tc/Neurology.jpg" alt="" className='specialities__IndImage' />
-        </div>
-        <div>
-            <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,ar_0.8,fl_progressive,f_auto,q_auto:eco/dpr_2/image/singles/care/tc/Rheumatology.png" alt="" className='specialities__IndImage' />
-        </div>
-        <div>
-            <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,ar_0.8,fl_progressive,f_auto,q_auto:eco/dpr_2/image/singles/care/tc/Cardiology.jpg" alt="" className='specialities__IndImage' />
-        </div>
-        <div>
-            <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,ar_0.8,fl_progressive,f_auto,q_auto:eco/dpr_2/image/singles/care/tc/Oncologist.png" alt="" className='specialities__IndImage' />
-        </div>
-        <div>
-            <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,ar_0.8,fl_progressive,f_auto,q_auto:eco/dpr_2/image/singles/care/tc/Nephrology.jpg" alt="" className='specialities__IndImage' />
-        </div>
+      <div className='superspecialists__images'>
+            <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,ar_0.8,fl_progressive,f_auto,q_auto:eco/dpr_2/image/singles/care/tc/GastroEnterologist.png" alt="" className='superspecialists__IndImage' />
+            <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,ar_0.8,fl_progressive,f_auto,q_auto:eco/dpr_2/image/singles/care/tc/ENDO.jpg" alt="" className='superspecialists__IndImage' />
+            <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,ar_0.8,fl_progressive,f_auto,q_auto:eco/dpr_2/image/singles/care/tc/Neurology.jpg" alt="" className='superspecialists__IndImage' />
+            <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,ar_0.8,fl_progressive,f_auto,q_auto:eco/dpr_2/image/singles/care/tc/Rheumatology.png" alt="" className='superspecialists__IndImage' />
+            <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,ar_0.8,fl_progressive,f_auto,q_auto:eco/dpr_2/image/singles/care/tc/Cardiology.jpg" alt="" className='superspecialists__IndImage' />
+            <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,ar_0.8,fl_progressive,f_auto,q_auto:eco/dpr_2/image/singles/care/tc/Oncologist.png" alt="" className='superspecialists__IndImage' />
+            <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/w_232,ar_0.8,fl_progressive,f_auto,q_auto:eco/dpr_2/image/singles/care/tc/Nephrology.jpg" alt="" className='superspecialists__IndImage' />
       </div>
     </div>
   )
