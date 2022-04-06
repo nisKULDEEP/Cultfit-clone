72fa5092fb2214d64ec7c4a23b6c2ed3190f2bd3
Akash Patil <patilakash211@gmail.com>
2022-04-06 22:06:20 +0530

    fitnesscompleted

---
+++ src/Components/Fitness/Navbar2.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Navbar2.css";
+
+export const Navbar2 = () => {
+  return (
+     <div className='second-nav'>
+           <a className='nav-link'>CultpassELITE</a>
+           <a className='nav-link' >CultpassPRO</a>
+           <a className='nav-link'>CultpassLIVE</a>
+           <a className='nav-link'>CultpassTransform</a>       
+     </div>
+  )
+}
