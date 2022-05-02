68c3cd53dfda36309f1e1bc9fb5d9e48489ab5fd
Akash Patil <patilakash211@gmail.com>
2022-05-02 09:28:55 +0530

    Css changes

--- src/Components/Fitness/Navbar2.jsx
+++ src/Components/Fitness/Navbar2.jsx
@@ -4,10 +4,10 @@ import "./Navbar2.css";
 export const Navbar2 = () => {
   return (
      <div className='second-nav'>
-           <a className='nav-link'>CultpassELITE</a>
-           <a className='nav-link' >CultpassPRO</a>
-           <a className='nav-link'>CultpassLIVE</a>
-           <a className='nav-link'>CultpassTransform</a>       
+           <a className='nav-link' href='CultpassELITE'>CultpassELITE</a>
+           <a className='nav-link'href='CultpassPRO' >CultpassPRO</a>
+           <a className='nav-link' href='CultpassLIVE'>CultpassLIVE</a>
+           <a className='nav-link' href='CultpassTransform'>CultpassTransform</a>       
      </div>
   )
 }
