0e07f3cca5dd2c50707b721d3e53c7e3108aeeb9
niskuldeep <kuldeepnishad.singh@gmail.com>
2022-05-04 09:37:09 +0530

    sdf

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import {
 
 import { Routes, Route } from "react-router-dom";
 
+// import Error from "./Components/Error/Error";
 // import Cart from "./Components/Cart/Cart";
 
 function App() {
@@ -30,6 +31,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/products/:id" element={<Product />} />
+        <Route path="/*" element={<Error />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="*" element={<Error />} />
       </Routes>
