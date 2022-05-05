74ab8e6fc258f50661ac77a36b1ec0d4a43d2882
niskuldeep <kuldeepnishad.singh@gmail.com>
2022-05-05 21:53:33 +0530

    authO

--- src/App.js
+++ src/App.js
@@ -16,11 +16,35 @@ import {
 
 import { Routes, Route } from "react-router-dom";
 import ProfilePage from "./Components/UserProfile/Profile";
+import { useEffect, useState } from "react";
 
 // import Error from "./Components/Error/Error";
 // import Cart from "./Components/Cart/Cart";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://127.0.0.1:3030/auth/login/success`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": true,
+      },
+    })
+      .then(
+        (res) => res.json()
+        // throw new Error("Authentification failed");
+      )
+      .then((res) => {
+        console.log(res);
+        setUser(res.user);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(user);
   return (
     <div>
       <Routes>
